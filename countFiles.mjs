0cf143ed95e0beccc129fde6648bb3ec469c83af
Alexander Smirnov <priamus.rex@gmail.com>
2025-07-10 13:32:04 +0300

    add lodash

---
+++ countFiles.mjs
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+
+const directoryPath = 'node_modules/lodash/'; // Укажите путь к вашей папке
+
+function countFiles(dir) {
+  let fileCount = 0;
+
+  fs.readdir(dir, (err, files) => {
+    if (err) {
+      return console.error('Ошибка при чтении директории:', err);
+    }
+
+    files.forEach(file => {
+      const filePath = path.join(dir, file);
+      if (fs.statSync(filePath).isFile()) {
+        fileCount++;
+      }
+    });
+
+    console.log(`Количество файлов в папке "${dir}": ${fileCount}`);
+  });
+}
+
+countFiles(directoryPath);
