94ae9677ca4aef1c0eab71f8dbe9d5a462ba3ccc
Alexander Smirnov <priamus.rex@gmail.com>
2025-07-12 22:17:57 +0300

    change of entry point

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-console.log('Hello, Hexlet!');
-import _ from 'lodash';
-console.log(_.last(['one', 'two']));
+import getBirthYear from 'hexlet';
+
+console.log('Сколько вам лет?')
+const age = 18 // Каким-то способом получаем возраст пользователя, через терминал. Сейчас не важно как
+const birthYear = getBirthYear(age)
+console.log(`Ваш год рождения: ${birthYear}`)
